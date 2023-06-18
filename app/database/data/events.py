660d6577af11127000b22bbb09d8407c4ad572b9
gayanDeeegala <gayandeegala@gmail.com>
2023-06-18 10:13:04 +0530

    data corrected

--- app/database/data/events.py
+++ app/database/data/events.py
@@ -29,18 +29,30 @@ movieTitles: List[MovieTitle] = [
     MovieTitle.TheLittleMermaid,
 ]
 
+dummy_bookings = ['C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'E8', 'E9', 'E10', 'E11', 'E12', 'E13', 'G10', 'G11', 'J5',
+                  'J6', 'J7', 'J8', 'J9', 'J10', 'J11', 'J12', 'J13']
+
 events_data = []
 i = 0
 for date in Date:
     for showTime in ShowTime:
         for theatre in Theatre:
             theater_name = Theatre(theatre).value
-            event = (
-                date.value,
-                showTime.value,
-                theater_name,
-                movieTitles[i % len(movieTitles)].value,
-                json.dumps(['A9', 'A10', 'A8', 'C10']),
-            )
+            if theatre == Theatre.THEATRE_1:
+                event = (
+                    date.value,
+                    showTime.value,
+                    theater_name,
+                    movieTitles[i % len(movieTitles)].value,
+                    json.dumps(dummy_bookings),
+                )
+            else:
+                event = (
+                    date.value,
+                    showTime.value,
+                    theater_name,
+                    movieTitles[i % len(movieTitles)].value,
+                    json.dumps([]),
+                )
             events_data.append(event)
             i += 1
